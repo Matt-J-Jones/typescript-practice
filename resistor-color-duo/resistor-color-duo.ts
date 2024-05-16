606bb695130fcdcd658e6ed5350aa4851985be30
Matt Jones <hello@mttjns.co.uk>
2024-05-16 18:18:03 +0100

    Challenge complete

--- resistor-color-duo/resistor-color-duo.ts
+++ resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,16 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
+export function decodedValue(colours: string[]): number|undefined {
+  return parseInt(colourValues[colours[0]].toString() + colourValues[colours[1]].toString());
+}
+
+const colourValues: {[key: string]: number} = {
+  'black': 0,
+  'brown': 1,
+  'red': 2,
+  'orange': 3,
+  'yellow': 4,
+  'green': 5,
+  'blue': 6,
+  'violet': 7,
+  'grey': 8,
+  'white': 9
 }
