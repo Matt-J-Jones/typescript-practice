0bcf231b86db87a4f5476abf03efdf5e3a0be2a5
Matt Jones <hello@mttjns.co.uk>
2024-05-16 18:04:29 +0100

    Challenge Complete - Resistor Colours

---
+++ resistor-color/resistor-color.ts
@@ -0,0 +1,28 @@
+export const colorCode = (colour: string): number | undefined => {
+  const colourValues: {[key: string]: number} = {
+    'black': 0,
+    'brown': 1,
+    'red': 2,
+    'orange': 3,
+    'yellow': 4,
+    'green': 5,
+    'blue': 6,
+    'violet': 7,
+    'grey': 8,
+    'white': 9
+  }
+  return colourValues[colour];
+}
+
+export const COLORS: string[] = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+];
