53044f55e6b6d08c45b55ebf92d6da62177b1240
Matt Jones <hello@mttjns.co.uk>
2024-05-17 17:55:42 +0100

    exercise complete

--- rna-transcription/rna-transcription.ts
+++ rna-transcription/rna-transcription.ts
@@ -1,3 +1,21 @@
-export function toRna() {
-  throw new Error('Remove this statement and implement this function')
+export function toRna(rna: string): string {
+  if (rna.match(/[^GCTA]/)) {
+    throw new Error('Invalid input DNA.');
+  }
+  
+  const rnaDict: {[key: string]: string} = {
+    'G': 'C',
+    'C': 'G',
+    'T': 'A',
+    'A': 'U'
+  }
+
+  let returnString = '';
+
+  for (const char of rna) {
+    returnString += rnaDict[char];
+  }
+  
+  
+  return returnString;
 }
