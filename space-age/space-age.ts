77696725ed799e63be1e5c23546c179f85b56461
Matt Jones <hello@mttjns.co.uk>
2024-05-17 18:18:42 +0100

    exercise complete

--- space-age/space-age.ts
+++ space-age/space-age.ts
@@ -1,3 +1,15 @@
-export function age(planet: unknown, seconds: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function age(planet: string, seconds: number): number {
+  const earthYear = 31557600;
+  const planets: {[key: string]: number} = {
+    'earth': 1,
+    'mercury': 0.2408467,
+    'venus': 0.61519726,
+    'mars': 1.8808158,
+    'jupiter': 11.862615,
+    'saturn': 29.447498,
+    'uranus': 84.016846,
+    'neptune': 164.79132
+  };
+
+  return Number((seconds / (planets[planet] * earthYear)).toFixed(2));
 }
