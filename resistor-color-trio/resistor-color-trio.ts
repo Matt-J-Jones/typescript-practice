7c639febd95f2abf7017c4a0afeec3169f00e090
Matt Jones <hello@mttjns.co.uk>
2024-05-17 17:22:57 +0100

    Excersise Complete

--- resistor-color-trio/resistor-color-trio.ts
+++ resistor-color-trio/resistor-color-trio.ts
@@ -1,3 +1,41 @@
-export function decodedResistorValue() {
-  throw new Error('Remove this statement and implement this function')
+export function decodedResistorValue(colours: string[]): string | undefined{
+  return label(colours);
+}
+
+const COLORS_DICT: {[key: string]: number} = {
+  'black': 0,
+  'brown': 1,
+  'red': 2,
+  'orange': 3,
+  'yellow': 4,
+  'green': 5,
+  'blue': 6,
+  'violet': 7,
+  'grey': 8,
+  'white': 9
+}
+
+function label(colors: string[]): string {
+  const numberStr = COLORS_DICT[colors[0]].toString()
+      + COLORS_DICT[colors[1]].toString()
+      + '0'.repeat(COLORS_DICT[colors[2]]);
+
+  return convertResults(numberStr);
+}
+
+function convertResults(numberStr: string): string {
+  const ohmsPrefix = ['ohms', 'kiloohms', 'megaohms', 'gigaohms'];
+  let count = 0;
+
+  for (const char of numberStr) {
+      if (char === '0') {
+          count++;
+      }
+  }
+
+  for (let x = 0; x < Math.floor(count / 3); x++) {
+      numberStr = numberStr.replace(/000/g, '');
+  }
+
+  return parseInt(numberStr, 10) + " " + ohmsPrefix[Math.floor(count / 3)];
 }
